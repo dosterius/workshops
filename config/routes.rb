e9b38c7d9193c549fcfebdcc47d557c09e8cbf93
Naimad <doster@op.pl>
2018-02-18 14:23:30 +0100

    Add User show

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,13 @@
 Rails.application.routes.draw do
   get "/test", to: proc { |e| [200, {}, ["ok"]]}
 
-  get "/users", to: "users#index"
+  #get "/users", to: "users#index"
   get "/contact", to: "static_pages#contact"
   get "/about", to: "static_pages#about_us"
   get "/mission", to: "static_pages#mission"
   get "/kariera", to: "static_pages#kariera"
 
   resources :orders
-
+  resources :users
 
 end
