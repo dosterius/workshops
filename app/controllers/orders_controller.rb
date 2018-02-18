68b226921e98ca81be4bd4613d2397ec44668a36
Naimad <doster@op.pl>
2018-02-18 17:35:50 +0100

    Add form

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -5,8 +5,24 @@ class OrdersController < ApplicationController
 
 	end
 
+	def new
+			@order = current_user.orders.new
+	end
+
+	def create
+		current_user.orders.create(order_params)
+		redirect_to user_orders_path(current_user)
+	end
+
 	def show
 		@order = Order.find(params[:id])
 	end 
+
+	private
+
+	def order_params
+		params.require(:order).permit(:name)
+	end
+
 
 end
