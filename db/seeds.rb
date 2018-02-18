02f0da7c444682edd3b00b77646073b5072eca37
Naimad <doster@op.pl>
2018-02-18 10:37:28 +0100

    Add Seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+names = %w(Kasia Maciek Jane Patryk Kamil Czesiu Grzechu Mirek Mati Seba Adik)
+names.each { |name| User.create(name: name)}
